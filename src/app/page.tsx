1165c3f8acd0ae0e11995c5f1b930069e3a94f3c
Sahib <100060676+Blackysh@users.noreply.github.com>
2023-06-25 20:18:46 +0530

    asd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,16 @@ import logo from './logo.png'
 import TextTransition, { presets } from 'react-text-transition';
 import { motion } from 'framer-motion';
 import React from 'react';
+import youtube from './images/youtube-symbol1.png'
+import dc from './images/dc.png'
+import spoti from './images/spoti.png'
+import git from './images/git.png'
+import twit from './images/Twit.png'
+import insta from './images/insta.png'
+import red from './images/red.png'
+import pin from './images/pin.png'
+import mail from './images/mail.png'
+import Iframe from 'react-iframe'
 
 const TEXTS = ['Create', 'Code', 'Design'];
 
@@ -78,14 +88,48 @@ export default function Home() {
 
 
           <div className='imgname '>
-            <Image src={logo} className='logo'></Image>
+             <Image alt='alternate text' src={logo} className='logo'></Image>
             <h1 className='name'>Arqies</h1>
           </div>
+          <div className='flex flex-col abmedia justify-center items-center'>
             <p className='aboutme'>Hi, I am Arqies. I am a developer who lives in India. I am constantly learning new things and like working on new and unique projects, while improving and expanding my knowledge of different things.</p>
+
+            <div className='flex gap-0'>
+            <li className="flex flex-warp gap-5 medialist">
+                
+                <a href="https://discord.arqies.com" target="_blank" ><Image alt='as' className="testcolor"src={dc} ></Image></a>
+                
+                <a href="mailto: contact@arqies.com" target="_blank" ><Image alt='as' className="testcolor"src={mail} ></Image></a>
+                <a href="https://github.arqies.com" target="_blank" ><Image alt='as' className="testcolor"src={git} ></Image></a>
+                <a href="https://instagram.arqies.com" target="_blank"><Image alt='as' className="testcolor"src={insta} ></Image></a>
+              
+                <a href="https://spotify.arqies.com"target="_blank" ><Image alt='as' className="testcolor"src={spoti} ></Image></a>
+
+                <a href="https://twitter.arqies.com" target="_blank"><Image alt='as' className="testcolor"src={twit} ></Image></a>
+                <a href="https://youtube.arqies.com" target="_blank"> <Image alt='as' className="testcolor"src={youtube} ></Image></a>
+
+            </li>
+
+            </div>
+            </div>
+
+
+        </motion.div>
+
+<div className='dls bg-slate-900'>
+        <Iframe url="https://arqies.com"
+        width="100%"
+        height="720px"
+        id=""
+        className="dl"
+        display="block"
+        position="relative"/>
+</div>
+
+
 
             </motion.div>
 
-        </motion.div>
 
       </div>
 
