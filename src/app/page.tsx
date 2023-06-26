f94cd2ade6b77f33be4b21f75a4f22e5ba8c1613
Sahib <100060676+Blackysh@users.noreply.github.com>
2023-06-26 12:32:32 +0530

    scrollbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between">
 
       
-      <div className='maindiv' style={{minHeight:'100%', minWidth: '100%'}}>
+      <div className='maindiv scrollbar' id='scrollbar' style={{minHeight:'100%', minWidth: '100%'}}>
 
   <motion.div className='bigtext flex p-0' initial="hidden" animate="visible" variants={{
   hidden: {
