f225b0dfea647ee6fdc7f7b94d7bea93c6a40006
Sahib <100060676+Blackysh@users.noreply.github.com>
2023-06-29 13:31:14 +0530

    poiuytrewqasdfghjklmnbvcxz

--- src/app/hires.tsx
+++ src/app/hires.tsx
@@ -14,7 +14,7 @@ import red from './images/red.png'
 import pin from './images/pin.png'
 import './hires.css'
 import mail from './images/mail.png'
-const TEXTS = ['Website?', 'Software?', 'Bot?' , 'Plugin?'];
+const TEXTS = ['Website?', 'Software?', 'Bot?'];
 
 
 export default function Hire(){
